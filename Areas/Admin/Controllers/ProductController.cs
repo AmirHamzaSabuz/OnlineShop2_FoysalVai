1eade7b869af36bbfbb78bc8b9f5265b2536341a
Amir Hamza <hamza_amir@yahoo.com>
2023-01-11 23:10:35 +0600

    Product Index

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop2.Data;
+
+namespace OnlineShop2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _db.Products.Include(x=> x.ProductType).Include(y=> y.SpecialTag).ToList();
+            return View(products);
+        }
+    }
+}
