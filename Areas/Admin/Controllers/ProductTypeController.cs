b8885b1264c3e9d0106f1caceb926683139b6681
Amir Hamza <hamza_amir@yahoo.com>
2023-01-11 15:26:53 +0600

    ProductType Controller Delete method and view

--- Areas/Admin/Controllers/ProductTypeController.cs
+++ Areas/Admin/Controllers/ProductTypeController.cs
@@ -89,6 +89,50 @@ namespace OnlineShop2.Areas.Admin.Controllers
 
         }
 
-        
+        // Delete Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            //var productType = _db.ProductTypes.Find(id);
+            var productType = _db.ProductTypes.FirstOrDefault(x => x.Id == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+
+        }
+
+        //Delete Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(/*int id,*/ ProductType productType)
+        {
+            //if (id == null)
+            //{
+            //    return NotFound();
+            //}
+            //if (id != productType.Id)
+            //{
+            //    return NotFound();
+            //}
+            //var product_Type = _db.ProductTypes.FirstOrDefault(x => x.Id == id);
+            //if (product_Type == null)
+            //{
+            //    return NotFound();
+            //}
+            //if (ModelState.IsValid)
+            //{
+            //_db.ProductTypes.Remove(product_Type);
+            _db.ProductTypes.Remove(productType);
+            await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            //}
+            //return View(productType);
+        }
+
     }
 }
