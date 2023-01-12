d118a70e07843b5cffb5e8555fcd6a9de853a1f1
Amir Hamza <hamza_amir@yahoo.com>
2023-01-12 08:31:48 +0600

    Create Get method without Image

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop2.Data;
+using OnlineShop2.Models;
 
 namespace OnlineShop2.Areas.Admin.Controllers
 {
@@ -18,6 +19,23 @@ namespace OnlineShop2.Areas.Admin.Controllers
         {
             var products = _db.Products.Include(x=> x.ProductType).Include(y=> y.SpecialTag).ToList();
             return View(products);
+        }
+
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product )
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
         }
     }
 }
